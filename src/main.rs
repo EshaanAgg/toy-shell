913c6d9f11147ce57bdba4847fd77e6de5bdce09
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2024-05-30 16:06:14 +0530

    make repl

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod cmd;
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    cmd::execute(&input);
+        cmd::execute(&input);
+    }
 }
