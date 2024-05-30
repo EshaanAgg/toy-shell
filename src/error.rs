6433611132117d404f0e4d9caa184fd535b5a75a
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2024-05-30 17:14:32 +0530

    codecrafters submit [skip ci]

--- src/error.rs
+++ src/error.rs
@@ -2,9 +2,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum AppError<'a> {
-    #[error("Error: {0}")]
-    GeneralError(&'a str),
-
+    // #[error("Error: {0}")]
+    // GeneralError(&'a str),
     #[error("Invalid Argument '{0}': {1}")]
     ArgsError(&'a str, &'a str),
 }
