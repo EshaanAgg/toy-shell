695ac74c82e3939d53174d429c9ba57a78033a90
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2024-05-30 16:13:36 +0530

    add functionality to exit

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,21 +1,43 @@
 enum Commands<'a> {
+    Exit(i32),
     Unknown(&'a str),
 }
 
-impl<'a> Commands<'a> {
-    fn from_str(s: &'a str) -> Self {
-        let trimmed_cmd = s.trim();
+struct Error {
+    message: String,
+}
 
-        match trimmed_cmd {
-            _ => Commands::Unknown(trimmed_cmd),
+impl<'a> Commands<'a> {
+    fn from_str(s: &'a str) -> Result<Self, Error> {
+        let cmd_parts = s.trim().split(" ").collect::<Vec<&str>>();
+
+        match cmd_parts[0] {
+            "exit" => {
+                let code = if cmd_parts.len() > 1 {
+                    match cmd_parts[1].parse::<i32>() {
+                        Ok(c) => c,
+                        Err(_) => {
+                            return Err(Error {
+                                message: "Invalid exit code".to_string(),
+                            })
+                        }
+                    }
+                } else {
+                    0
+                };
+                Ok(Commands::Exit(code))
+            }
+            _ => Ok(Commands::Unknown(s.trim())),
         }
     }
 }
 
 pub fn execute(input: &str) {
     match Commands::from_str(input) {
-        Commands::Unknown(s) => {
-            println!("{}: command not found", s);
-        }
+        Ok(cmd) => match cmd {
+            Commands::Exit(code) => std::process::exit(code),
+            Commands::Unknown(cmd) => println!("Unknown command: {}", cmd),
+        },
+        Err(e) => println!("Error: {}", e.message),
     }
 }
